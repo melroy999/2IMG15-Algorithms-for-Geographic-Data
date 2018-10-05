6efd8ebcb5095d014834da578d7077ee77c3b788
Nijnatten <m.v.nijnatten@student.tue.nl>
2018-10-05 13:17:18 +0200

    [/Outlines] Improved the line intersection for endpoints.

--- src/agd/data/sweepline2/LineSegment.java
+++ src/agd/data/sweepline2/LineSegment.java
@@ -25,7 +25,7 @@ public class LineSegment implements Comparable<LineSegment> {
         this.edge = edge;
 
         // Make sure that the left and right endpoints are oriented correctly.
-        if(edge.getDirection() == OutlineEdge.Direction.UP || edge.getDirection() == OutlineEdge.Direction.RIGHT) {
+        if (edge.getDirection() == OutlineEdge.Direction.UP || edge.getDirection() == OutlineEdge.Direction.RIGHT) {
             left = edge.getOrigin();
             right = edge.getTarget();
         } else {
@@ -43,30 +43,29 @@ public class LineSegment implements Comparable<LineSegment> {
      */
     @SuppressWarnings("Duplicates")
     public boolean intersects(LineSegment ls) {
-        if(ls == null) return false;
+        if (ls == null) return false;
 
-        if(edge.getDirection().isHorizontal != ls.edge.getDirection().isHorizontal) {
+        if (edge.getDirection().isHorizontal != ls.edge.getDirection().isHorizontal) {
             // Get the intersection point of the two infinite lines of which this and ls are segments.
             Point2d i = intersectionPoint(ls);
 
             // The intersection point should not be equivalent to two of the endpoints.
-            long noEquals = Stream.of(i.epsilonEquals(left, 1e-4), i.epsilonEquals(right, 1e-4),
-                    i.epsilonEquals(ls.left, 1e-4), i.epsilonEquals(ls.right, 1e-4)).filter(b -> b).count();
-            if(noEquals > 1) {
+            if ((i.epsilonEquals(left, 1e-4) || i.epsilonEquals(right, 1e-4)) &&
+                    (i.epsilonEquals(ls.left, 1e-4) || i.epsilonEquals(ls.right, 1e-4))) {
                 return false;
             }
 
             // Check if the point is on one of the lines.
-            if(edge.getDirection().isHorizontal) {
+            if (edge.getDirection().isHorizontal) {
                 // By construction of a line segment, the left will have the lowest x and y.
                 double x = ls.left.x;
-                if(x < left.x || x > right.x) return false;
+                if (x < left.x || x > right.x) return false;
                 double y = left.y;
                 return !(y < ls.left.y) && !(y > ls.right.y);
             } else {
                 // The y value is a range, x is constant.
                 double x = left.x;
-                if(x < ls.left.x || x > ls.right.x) return false;
+                if (x < ls.left.x || x > ls.right.x) return false;
                 double y = ls.left.y;
                 return !(y < left.y) && !(y > right.y);
             }
@@ -103,7 +102,7 @@ public class LineSegment implements Comparable<LineSegment> {
      * @return The intersection point between the two line segments.
      */
     public Point2d intersectionPoint(LineSegment ls) {
-        if(edge.getDirection().isHorizontal) {
+        if (edge.getDirection().isHorizontal) {
             return new Point2d(ls.left.x, left.y);
         } else {
             return new Point2d(left.x, ls.left.y);
